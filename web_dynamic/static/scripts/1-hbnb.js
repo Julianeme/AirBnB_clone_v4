218c7ef7deb47d6b8f202091bc4b704cf863447a
Julianeme <juliantve@gmail.com>
2021-09-24 16:02:07 -0500

    pushing small grammar corrections on html files

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,9 @@
 $(document).ready(function () {
   let amty_id = {}
   if ($("input[type=checkbox]").prop(":checked")) {
-    amty_id.append($(this).find('li'))
-
-    $($('.advancedSearchCheckBoxList')).each(function () {// id of ul
-      var li = $(this).find('li')//get each li in ul
-      li.each(function () {// id of ul
-        labels.push($(this).text())
-      })
-    })
-
-  else 
+    amty_id[$(this).data('id')] = $(this).data('name');
+  }) else {
+  delete amty_id[$(this).find('id')]
   }
-  input type = "checkbox"// Your code goes here
-});
+}
+
