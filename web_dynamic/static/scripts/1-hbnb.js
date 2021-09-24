2878c43fc802a17a3ef8e1700cc155e9ae6504ba
Julianeme <juliantve@gmail.com>
2021-09-24 14:22:30 -0500

    1-hbnb.js 1-hbnb.html | adding files

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
   let amty_id = {}
   if ($("input[type=checkbox]").prop(":checked")) {
-    amty_id[id] = this.$data
+    amty_id.append($(this).find('li'))
+
+    $($('.advancedSearchCheckBoxList')).each(function () {// id of ul
+      var li = $(this).find('li')//get each li in ul
+      li.each(function () {// id of ul
+        labels.push($(this).text())
+      })
+    })
+
+  else 
   }
   input type = "checkbox"// Your code goes here
 });
