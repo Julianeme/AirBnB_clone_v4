46c79b4e72ad3cb5b5c8e3ac613cca94a1cdcdae
Julianeme <juliantve@gmail.com>
2021-09-24 13:57:09 -0500

    scripts and 1-hbnb.html | adding files

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  let amty_id = {}
+  if ($("input[type=checkbox]").prop(":checked")) {
+    amty_id[id] = this.$data
+  }
+  input type = "checkbox"// Your code goes here
+});
