bd7a2a6d0ccfdf108c7e77a71d8f7f5a4fff0a9f
daniapm <daniajibellis@hotmail.com>
2021-09-25 17:07:43 -0500

    check status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,25 +1,22 @@
 $(document).ready(function () {
-$(function ()
-{
-    $.get("http://0.0.0.0:5001/api/v1/status/", function (data, textStatus)
+
+    $.get("http://0.0.0.0:5001/api/v1/status/", function (data)
     {
-        if (textStatus === 'success'){
+        if (data.status === "OK"){
             $('#api_status').addClass('available');
         }
         else {
             $('#api_status').removeClass('available');
         }
     });
-});
 
-
-  let ampty_id = {}
-  $('input[type=checkbox]').change(function () {
-    if ($(this).is(':checked')) {
-      ampty_id[$(this).data('id')] = $(this).data('name');
-    } else {
-      delete ampty_id[$(this).data('id')];
-    }
-      $('.amenities h4').text(Object.values(ampty_id).join(', '));
-  });
+    let ampty_id = {}
+    $('input[type=checkbox]').change(function () {
+        if ($(this).is(':checked')) {
+        ampty_id[$(this).data('id')] = $(this).data('name');
+        } else {
+        delete ampty_id[$(this).data('id')];
+        }
+        $('.amenities h4').text(Object.values(ampty_id).join(', '));
+    });
 });
