14afd93780cb3a8291e67f917c86ae162aa677bb
Julianeme <juliantve@gmail.com>
2021-09-26 11:35:40 -0500

    3-hbnb.js partial implementation of post request to recover and display places

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -20,46 +20,26 @@ $(document).ready(function () {
   });
 
 $.ajax({
-
   // The URL for the request
   url: "http://0.0.0.0:5001/api/v1/places_search",
-
-  // The data to send (will be converted to a query string)
+  // The data to send (will be converted to a dictionary)
   data: {},
-
   // Whether this is a POST or GET request
   type: "POST",
-
   // The type of data we expect back
   dataType: "json",
   ContentType: 'application/json'
 })
   // Code to run if the request succeeds (is done);
   // The response is passed to the function
+  // Loop into the result of the request and create an article tag representing
+  // a Place in the section.places. (you can remove the Owner tag in the place description)
   .done(function (data) {
-
-    for recorriendo las clases segun el esquema del html siguiente:
-
-
-          <article></article>
-        <div class="title_box">
-          <h2>{{ place.name }}</h2>
-          <div class="price_by_night">${{ place.price_by_night }}</div>
-        </div>
-        <div class="information">
-          <div class="max_guest">{{ place.max_guest }} Guest{% if place.max_guest != 1 %}s{% endif %}</div>
-          <div class="number_rooms">{{ place.number_rooms }} Bedroom{% if place.number_rooms != 1 %}s{% endif
-            %}</div>
-          <div class="number_bathrooms">{{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1
-            %}s{% endif %}</div>
-        </div>
-        <div class="user">
-          <b>Owner:</b> {{ place.user.first_name }} {{ place.user.last_name }}
-        </div>
-        <div class="description">
-          {{ place.description | safe }}
-        </div>
-      </article >
+    let index = 0;
+    for (index = 0; index < data.length(); index ++){
+      place = data[index]
+      $('.places ').append('<article><h2>'{{ place.name }}'</h2><div class="price_by_night">$'{{ place.price_by_night }}'</div><div class="information"><div class="max_guest">'{{ place.max_guest }} 'Guest{% if place.max_guest != 1 %}s{% endif %}</div><div class="number_rooms">'{{ place.number_rooms }}'Bedroom{% if place.number_rooms != 1 %}s{% endif%}</div><div class="number_bathrooms">'{{ place.number_bathrooms }}'Bathroom{% if place.number_bathrooms != 1%}s{% endif %}</div></div><div class="user"><b>Owner:</b>'{{ place.user.first_name }} {{ place.user.last_name }}'</div><div class="description">'{{ place.description | safe }}'</div></article>)
+    }
   })
   // Code to run if the request fails; the raw request and
   // status codes are passed to the function
